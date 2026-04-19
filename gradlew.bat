edbac4564e5e42fbb1734fec909d8ec817972412
K4MIKAZ3 <123911892+K4MIKAZ3@users.noreply.github.com>
2026-04-19 12:24:02 -0500

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@rem Gradle wrapper script for Windows
-@echo off
-setlocal
-
-set APP_HOME=%~dp0
-set JAVA_EXE=java.exe
-
-%JAVA_EXE% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
